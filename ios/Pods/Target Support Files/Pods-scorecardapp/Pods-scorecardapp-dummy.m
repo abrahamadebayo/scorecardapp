98b844ae5c6eff04edaa798fafd7a445e8898d8c
Quadri Adebayo <adebayo.abraham@gmail.com>
2020-02-02 22:21:54 +0100

    Installation of default react template

---
+++ ios/Pods/Target Support Files/Pods-scorecardapp/Pods-scorecardapp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_scorecardapp : NSObject
+@end
+@implementation PodsDummy_Pods_scorecardapp
+@end
---
+++ ios/Pods/Target Support Files/Pods-scorecardappTests/Pods-scorecardappTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_scorecardappTests : NSObject
+@end
+@implementation PodsDummy_Pods_scorecardappTests
+@end
